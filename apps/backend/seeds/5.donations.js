3139bb928b7b4bcddef836f0c656a8d7aa6d0aed
Oleh Melnyk <olehmelnyk@iMac-Oleh.local>
2022-12-13 00:40:24 +0200

    add proper donations seed

--- apps/backend/seeds/5.donations.js
+++ apps/backend/seeds/5.donations.js
@@ -1,16 +1,23 @@
+const { faker } = require('@faker-js/faker');
+
 module.exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('DonationsHistory').del();
 
-  // TODO: get list of active campaigns and it's ids
+  const activeCampaigns = (
+    await knex('Campaigns').select('id').where('status', 'active')
+  ).map(({ id }) => id);
 
-  // await knex('DonationsHistory').insert([
-  //   {
-  //     amount: 100.54,
-  //     fiat_currency: 'USD',
-  //     nickname: 'John',
-  //     state: 'valid',
-  //     campaign: '',
-  //   },
-  // ]);
+  // console.log('activeCampaigns', activeCampaigns);
+
+  const donations = Array.from({ length: 10000 }, () => ({
+    amount: faker.datatype.number({ min: 10, max: 50000, precision: 0.01 }),
+    fiat_currency: faker.helpers.arrayElement(['USD', 'EUR', 'UAH']),
+    nickname: faker.internet.userName(),
+    state: 'valid',
+    campaign:
+      activeCampaigns[Math.floor(Math.random() * activeCampaigns.length)],
+  }));
+
+  await knex('DonationsHistory').insert(donations);
 };
