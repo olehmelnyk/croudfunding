8b6575489db42502205a8f7d313b490ecf20ebd4
Oleh Melnyk <olehmelnyk@iMac-Oleh.local>
2022-12-12 02:09:28 +0200

    WIP: add template for db seed for donation history

---
+++ apps/backend/seeds/5.donations.js
@@ -0,0 +1,16 @@
+module.exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('DonationsHistory').del();
+
+  // TODO: get list of active campaigns and it's ids
+
+  await knex('DonationsHistory').insert([
+    {
+      amount: 100.54,
+      fiat_currency: 'USD',
+      nickname: 'John',
+      state: 'valid',
+      campaign: '',
+    },
+  ]);
+};
